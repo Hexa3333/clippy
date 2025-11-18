3310799229b27465558d217907d4f53e2cbf5760
Alp Yılmaz <hexa3333@protonmail.com>
2025-11-18 07:44:01 +0300

    concatenate signal handling call

--- main.cpp
+++ main.cpp
@@ -6,12 +6,35 @@
 #include <wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <signal.h>
 
 #include "device.hpp"
+
+// HOLY race condition...
+
+static int G_ffmpeg_instance_log_fd = -1;
+static int G_ffmpeg_instance_iter = 0;
+static pid_t G_ffmpeg;
 
 struct Config {
     int framerate;
 };
+
+static int G_concatenated = false;
+sighandler_t concatenate(int sig) {
+    std::cout << "SIGPOLL received. Clip will be created...\n";
+    kill(G_ffmpeg, SIGINT);
+
+    int wstatus;
+    waitpid(G_ffmpeg, &wstatus, 0);
+    std::cout << "[Master] ffmpeg exited with status: " << wstatus << ".\n";
+    ++G_ffmpeg_instance_iter;
+
+    // Read instance log fd and gather which clips should be thingied. Concat video.
+
+    G_concatenated = true;
+    return 0;
+}
 
 int main(int argc, char** argv) {
     Device device;
@@ -25,46 +48,43 @@ int main(int argc, char** argv) {
 
     Config config;
     config.framerate = 25;
+    
+    signal(SIGPOLL, (__sighandler_t)concatenate);
+
     // Device information is used for setting configs up
 
     mkdir("/tmp/clippy/", S_IRUSR | S_IWUSR | S_IXUSR);
-
-    int ffmpeg_instance_iter = 0;
     while (1) {
-        std::string ffmpeg_instance_log_filename = "/tmp/clippy/log-" + std::to_string(ffmpeg_instance_iter) + ".txt";
-        int ffmpeg_instance_log_fd = open(ffmpeg_instance_log_filename.c_str(), O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+        std::string ffmpeg_instance_log_filename = "/tmp/clippy/log-" + std::to_string(G_ffmpeg_instance_iter) + ".txt";
+        close(G_ffmpeg_instance_log_fd);
+        G_ffmpeg_instance_log_fd = open(ffmpeg_instance_log_filename.c_str(), O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
 
-        pid_t ffmpeg = fork();
-        if (ffmpeg == 0) {
+        G_ffmpeg = fork();
+        if (G_ffmpeg == 0) {
             std::cout << "[Child] forking ffmpeg instance...\n";
 
             // ffmpeg stderr to tmpfile
-            dup2(ffmpeg_instance_log_fd, STDERR_FILENO);
+            dup2(G_ffmpeg_instance_log_fd, STDERR_FILENO);
 
             // exec ffmpeg
             // ffmpeg -video_size 1920x1080 -f x11grab -i :0.0 -c:v libx264 -f segment -segment_format mp4 -segment_time 5 -reset_timestamps 1 -segment_wrap 10 /tmp/clippy-output-N-%01d.mp4
-            std::string ffmpeg_output_filename = "/tmp/clippy/capture-" + std::to_string(ffmpeg_instance_iter) + "-%01d.mp4";
+            std::string ffmpeg_output_filename = "/tmp/clippy/capture-" + std::to_string(G_ffmpeg_instance_iter) + "-%01d.mp4";
             execl("/usr/bin/ffmpeg", "ffmpeg", "-y", "-video_size", "1920x1080", "-f", "x11grab", "-i", ":0.0", "-c:v", "libx264", "-f", "segment", "-segment_format", "mp4", "-segment_time", "5", "-reset_timestamps", "1", "-segment_wrap", "10", ffmpeg_output_filename.c_str(), NULL);
 
             // exec fail
             std::cout << "[Child] ffmpeg exec failed: " << strerror(errno) << "\n";
-        } else if (ffmpeg == -1) {
+        } else if (G_ffmpeg == -1) {
             // Error
             std::cout << "[Master] fork failed: " << strerror(errno) << "\n";
             return -1;
         } else {
-            close(ffmpeg_instance_log_fd);
+            close(G_ffmpeg_instance_log_fd);
             std::cout << "[Master] Waiting on rec event.\n";
 
-            // While rec event not received, wait etc.
-            
-            // On receive, exec handler that concatenates video
-
-            // Quit and roll back
-            int wstatus;
-            waitpid(ffmpeg, &wstatus, 0);
-            std::cout << "[Master] ffmpeg exited with status: " << wstatus << ".\n";
-            ++ffmpeg_instance_iter;
+            // While rec event not received, wait
+            // TODO: smarter way to wait
+            while (!G_concatenated) {}
+            G_concatenated = false;
         }
     }
 }
